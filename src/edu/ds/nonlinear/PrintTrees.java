f225ae0c8cb502515a9f87a204afd402353d55ae
MohanS <smkrishnan1310@gmail.com>
2020-07-14 00:27:38 +0530

    Print Trees Spirally or in ZigZag fashion

--- src/edu/ds/nonlinear/PrintTrees.java
+++ src/edu/ds/nonlinear/PrintTrees.java
@@ -5,6 +5,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import edu.ds.linear.Stack;
+
 /**
  * Print Tree in below forms 1. Vertically 2. Spirally/ Zig-Zag
  * 
@@ -13,6 +15,7 @@ import java.util.Map;
  */
 public class PrintTrees<T> {
 
+	// Method to print tree vertically
 	public void printVertically(TNode<T> root) {
 		if (null != root) {
 			Map<Integer, List<T>> map = new HashMap<>();
@@ -22,6 +25,7 @@ public class PrintTrees<T> {
 
 	}
 
+	// Recursive call with left as -1 and right as 1
 	private void printVertically(TNode<T> root, Map<Integer, List<T>> map, int num) {
 		if (null != root) {
 			List<T> list = map.get(num);
@@ -35,6 +39,35 @@ public class PrintTrees<T> {
 		}
 	}
 
+	public void printSpirally(TNode<T> root) {
+		if (null != root) {
+			Stack<TNode<T>> s1 = new Stack<>();
+			Stack<TNode<T>> s2 = new Stack<>();
+
+			s1.push(root);
+			while (!s1.isEmpty() || !s2.isEmpty()) {
+				while (!s1.isEmpty()) {
+					TNode<T> node = s1.pop();
+					System.out.print(node.data + ", ");
+					if (null != node.left)
+						s2.push(node.left);
+					if (null != node.right)
+						s2.push(node.right);
+				}
+
+				while (!s2.isEmpty()) {
+					TNode<T> node = s2.pop();
+					System.out.print(node.data + ", ");
+					if (null != node.right)
+						s1.push(node.right);
+					if (null != node.left)
+						s1.push(node.left);
+				}
+			}
+
+		}
+	}
+
 	public static void main(String[] args) {
 		int arr[] = { 5, 3, 1, 2, 4, 8, 6, 9, 7, 10, 0 };
 		SimpleBinaryTree<Integer> tree = new SimpleBinaryTree<>((a, b) -> (b - a));
@@ -42,8 +75,13 @@ public class PrintTrees<T> {
 			tree.add(i);
 
 		PrintTrees<Integer> printer = new PrintTrees<>();
-		System.out.println("Print Trees vertically: ");
+		System.out.println("Print Trees vertically : ");
 		printer.printVertically(tree.root);
+
+		System.out.println("================");
+
+		System.out.println("Print Trees Spirally/ ZigZag : ");
+		printer.printSpirally(tree.root);
 
 	}
 
